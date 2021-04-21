d3fe65e3c0b69aef71ee2474436039b82d1a998f
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-04-21 18:42:21 +0100

    v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__img',{delay: 200}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
@@ -55,7 +55,3 @@ sr.reveal('.work__img',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
-
-
-
-
